1892a2e1d67433c7ccd908cc68ddd86eaeb9288e
suri.kim <suri.kim@daumkakao.com>
2018-06-17 22:05:29 +0900

    LeNet 구현해서 학습시켜보기 - sigmoid대신 ReLU 사용하고 평균 풀링대신 맥스 풀링 사용하도록 변경

---
+++ network/half_float_network.py
@@ -0,0 +1,26 @@
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+from network.lenet import LeNet
+from dataset.mnist import load_mnist
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+
+network = LeNet()
+network.load_params("lenet_params.pkl")
+
+sampled = 10000 # 고속화를 위한 표본추출
+x_test = x_test[:sampled]
+t_test = t_test[:sampled]
+
+print("caluculate accuracy (float64) ... ")
+print(network.accuracy(x_test, t_test))
+
+# float16(반정밀도)로 형변환
+x_test = x_test.astype(np.float16)
+for param in network.params.values():
+    param[...] = param.astype(np.float16)
+
+print("caluculate accuracy (float16) ... ")
+print(network.accuracy(x_test, t_test))
